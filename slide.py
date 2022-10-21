628d6036383f7f984a26c7d0f94fd3851ca1e3ff
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-10-22 01:07:34 +0530

    Fixed Indentation

--- slide.py
+++ slide.py
@@ -169,8 +169,6 @@ def main():
                 elif event.key in (K_DOWN, K_s) and isValidMove(mainBoard, DOWN):
 
                     Move = DOWN
-            elif event.type == QUIT:
-                running = False
 
 
                 elif event.key == K_r:
@@ -205,6 +203,9 @@ def main():
                     mainBoard = getStartingBoard()
                     SOLVEDBOARD = getStartingBoard()
                     solutionSeq = []
+                    
+            elif event.type == QUIT:
+                running = False
 
         if Move:
 
